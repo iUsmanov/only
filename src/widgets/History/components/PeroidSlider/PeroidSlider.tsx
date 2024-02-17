9b6f00bbacb21a343c610e80ce3b393bfb451bec
Idris <my-git-hub@mail.ru>
2024-02-17 15:13:17 +0300

    useDynamicNumber

--- src/widgets/History/components/PeroidSlider/PeroidSlider.tsx
+++ src/widgets/History/components/PeroidSlider/PeroidSlider.tsx
@@ -5,17 +5,21 @@ import { Wheel } from '../Wheel/Wheel';
 import { useSelector } from 'react-redux';
 import { getYears } from '../../model/selectors/getYears';
 import { WheelNav } from '../WheelNav/WheelNav';
+import { useDynamicNumber } from '../../lib/hooks/useDynamicNumber/useDynamicNumber';
 
 export const PeroidSlider = memo((props: { className?: string }) => {
 	const { className } = props;
 	const years = useSelector(getYears);
+	const firstYear = useDynamicNumber({ time: 30, endNumber: years?.[0] });
+	const secondYear = useDynamicNumber({ time: 30, endNumber: years?.[1] });
 
 	if (!years) return null;
+
 	return (
 		<div className={classNames(cls.periodSlider, {}, [className])}>
 			<div className={cls.years}>
-				<div className={cls.firstYear}>{years[0]}</div>
-				<div className={cls.secondYear}>{years[1]}</div>
+				<div className={cls.firstYear}>{firstYear}</div>
+				<div className={cls.secondYear}>{secondYear}</div>
 				<Wheel />
 			</div>
 			<WheelNav className={cls.navigation} />
---
+++ src/widgets/History/lib/hooks/useDynamicNumber/useDynamicNumber.ts
@@ -0,0 +1,52 @@
+import { useEffect, useRef, useState } from 'react';
+
+interface UseDynamicNumberProps {
+	endNumber?: number;
+	time: number;
+}
+
+export const useDynamicNumber = (props: UseDynamicNumberProps) => {
+	const { endNumber, time } = props;
+	// Текущее отображаемое число
+	const [currentNumber, setCurrentNumber] = useState<number | undefined>(undefined);
+	// Число, с которого начинается новый отчёт
+	const startNumber = useRef<number | undefined>(undefined);
+	// Был ли вмонтирован компонент ?
+	const [wasOpened, setWasOpened] = useState<boolean>(false);
+
+	// Самая первая загрузка компонента
+	useEffect(() => {
+		// console.log('FIRST RENDER');
+		setCurrentNumber(endNumber);
+		startNumber.current = endNumber;
+		setWasOpened(true);
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
+
+	useEffect(() => {
+		// Если это не первая загрузка
+		if (wasOpened) {
+			// console.log('NEW RENDER');
+			setCurrentNumber(startNumber.current);
+			startNumber.current = endNumber;
+		}
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [endNumber]);
+
+	// Начинаем увеличивать число
+	useEffect(() => {
+		if (wasOpened) {
+			// console.log('CHANGE');
+			if (currentNumber && currentNumber !== endNumber && endNumber) {
+				if (currentNumber < endNumber) {
+					setTimeout(setCurrentNumber, time, currentNumber + 1);
+				} else if (currentNumber > endNumber) {
+					setTimeout(setCurrentNumber, time, currentNumber - 1);
+				}
+			}
+		}
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [endNumber, currentNumber]);
+
+	return currentNumber;
+};
